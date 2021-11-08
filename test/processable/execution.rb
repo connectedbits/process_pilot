273d7473be6f811d6ef99c74aa5239591d9d46f1
carlson <eric@connectedbits.com>
2021-11-08 16:48:36 -0500

    Bug fixes

--- test/processable/execution.rb
+++ test/processable/execution.rb
@@ -5,7 +5,7 @@ module Processable
     include ActiveModel::Model
     include ActiveModel::Serializers::JSON
 
-    attr_accessor :id, :status, :started_at, :ended_at, :variables, :tokens_in, :step_by_element_id, :start_event_id, :timer_expires_at, :message_names, :error_names, :condition
+    attr_accessor :id, :status, :started_at, :ended_at, :variables, :tokens_in, :tokens_out, :start_event_id, :timer_expires_at, :message_names, :error_names, :condition
     attr_accessor :step, :parent, :children, :context, :attached_to_id
 
     def self.start(context:, process_id:, variables: {}, start_event_id: nil, parent: nil)
@@ -37,7 +37,7 @@ module Processable
         @status ||= "activated"
         @variables ||= {}.with_indifferent_access
         @tokens_in ||= []
-        @.step_by_element_id ||= []
+        @tokens_out ||= []
         @message_names ||= []
         @error_names ||= []
         @children ||= []
@@ -270,28 +270,36 @@ module Processable
     def pretty_json
       JSON.pretty_generate(as_json)
     end
+  end
 
-    # def print
-    #   puts
-    #   puts "#{step.id} #{status} * #{tokens.join(', ')}"
-    #   print_variables unless variables.empty?
-    #   print_children
-    #   puts
-    # end
+  class Printer
+    attr_accessor :execution
 
-    # def print_children
-    #   puts
-    #   children.each_with_index do |child, index|
-    #     str = "#{index} #{child.step.type.split(':').last} #{child.step.id}: #{child.status} #{JSON.pretty_generate(variables, {indent: '', object_nl: ' ' }) unless child.variables.empty? }".strip
-    #     str = "#{str} * in: #{child.tokens_in.join(', ')}" if child.tokens_in.present?
-    #     str = "#{str} * out: #{child.tokens_out.join(', ')}" if child.tokens_out.present?
-    #     puts str
-    #   end
-    # end
+    def initialize(execution)
+      @execution = execution
+    end
 
-    # def print_variables
-    #   puts
-    #   puts JSON.pretty_generate(variables)
-    # end
+    def print
+      puts
+      puts "#{execution.step.id} #{execution.status} * #{execution.tokens.join(', ')}"
+      print_variables unless execution.variables.empty?
+      print_children
+      puts
+    end
+
+    def print_children
+      puts
+      execution.children.each_with_index do |child, index|
+        str = "#{index} #{child.step.type.split(':').last} #{child.step.id}: #{child.status} #{JSON.pretty_generate(variables, {indent: '', object_nl: ' ' }) unless child.variables.empty? }".strip
+        str = "#{str} * in: #{child.tokens_in.join(', ')}" if child.tokens_in.present?
+        str = "#{str} * out: #{child.tokens_out.join(', ')}" if child.tokens_out.present?
+        puts str
+      end
+    end
+
+    def print_variables
+      puts
+      puts JSON.pretty_generate(execution.variables)
+    end
   end
 end
