e8e66dcdce39fe159dd5e7cbdf215516e712f8a8
Elijah Miller <elijah.miller@gmail.com>
2022-06-06 11:23:28 -0400

    Minor cleanup.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
-
-ROOT = File.join(File.dirname(__FILE__), "..")
+GEM_ROOT = File.join(File.dirname(__FILE__), "..")
 
 require_relative "../lib/processable"
 
@@ -14,15 +11,11 @@ require "active_support/testing/time_helpers"
 
 Time.zone_default = Time.find_zone!("UTC")
 
-# ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/migrate", __dir__)]
-
-# # Load fixtures from the engine
-# if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-#   ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-#   ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-#   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-#   ActiveSupport::TestCase.fixtures :all
-# end
+Minitest::Reporters.use!(
+  Minitest::Reporters::ProgressReporter.new(color: true),
+    ENV,
+    Minitest.backtrace_filter,
+)
 
 class Minitest::Spec
   include ActiveSupport::Testing::TimeHelpers
@@ -32,14 +25,8 @@ class Minitest::Spec
 
   after :each do
   end
-end
 
-Minitest::Reporters.use!(
-  Minitest::Reporters::ProgressReporter.new(color: true),
-    ENV,
-    Minitest.backtrace_filter,
-)
-
-def fixture_source(filename)
-  File.read("#{ROOT}/test/fixtures/files/#{filename}")
+  def fixture_source(filename)
+    File.read(File.join(GEM_ROOT, "/test/fixtures/files", filename))
+  end
 end
