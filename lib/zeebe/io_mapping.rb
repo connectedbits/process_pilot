7648dd51f2d8731f803b678013f149bd2d86d45d
carlson <eric@connectedbits.com>
2023-11-27 16:07:18 -0500

    IO mapping checkpoint

--- lib/zeebe/io_mapping.rb
+++ lib/zeebe/io_mapping.rb
@@ -5,8 +5,8 @@ module Zeebe
     attr_accessor :inputs, :outputs
 
     def initialize(moddle)
-      @inputs = moddle["inputParameters"].map { |parameter_moddle| Parameter.new(parameter_moddle) }
-      @outputs = moddle["outputParameters"].map { |parameter_moddle| Parameter.new(parameter_moddle) }
+      @inputs = moddle["inputParameters"]&.map { |parameter_moddle| Parameter.new(parameter_moddle) }
+      @outputs = moddle["outputParameters"]&.map { |parameter_moddle| Parameter.new(parameter_moddle) }
     end
   end
 end
--- test/bpmn/io_mapping_test.rb
+++ test/bpmn/io_mapping_test.rb
@@ -2,29 +2,62 @@
 
 require "test_helper"
 
-module Bpmn
+module Processable
 
   describe "IO Mapping" do
     let(:source) { fixture_source("io_mapping.bpmn") }
     let(:context) { Processable::Context.new(sources: source) }
+    let(:services) {
+      {
+        greeti: proc { |_execution, variables|
+          { response: { body: { greeting: "👋 Hello #{variables[:name]}!" } } }
+        },
+      }
+    }
 
     describe :definition do
       let(:process) { context.process_by_id("IOMapping") }
       let(:start_event) { process.element_by_id("Start") }
-      let(:check_inventory_task) { process.element_by_id("CheckInventory") }
-      let(:order_product_task) { process.element_by_id("OrderProduct") }
+      let(:enter_name_task) { process.element_by_id("EnterName") }
+      let(:author_greeting_task) { process.element_by_id("AuthorGreeting") }
       let(:end_event) { process.element_by_id("End") }
 
       it "should parse the task" do
         _(start_event).wont_be_nil
-        _(check_inventory_task).wont_be_nil
-        _(order_product_task).wont_be_nil
+        _(enter_name_task).wont_be_nil
+        _(author_greeting_task).wont_be_nil
         _(end_event).wont_be_nil
       end
     end
 
     describe :execution do
+      let(:process) { @process }
+      let(:start_event) { process.child_by_step_id("Start") }
+      let(:enter_name_task) { process.child_by_step_id("EnterName") }
+      let(:author_greeting_task) { process.child_by_step_id("AuthorGreeting") }
+      let(:end_event) { process.child_by_step_id("End") }
 
+      before { @process = Execution.start(context: context, process_id: "IOMapping"); }
+
+      describe :mapping do
+        before { enter_name_task.signal({ first_name: "John", last_name: "Doe" }) }
+
+        describe :inputs do
+          it "should map the input" do
+            skip "TODO: implement io mapping in execution"
+            print_execution(process)
+            #_(enter_name_task.output[:name]).must_equal "John Doe"
+          end
+
+          # describe :outputs do
+          #   before { author_greeting_task.signal({}) }
+
+          #   it "should map the output" do
+          #     #_(author_greeting_task.output[:greeting]).must_equal "👋 Hello John Doe!"
+          #   end
+          # end
+        end
+      end
     end
   end
 end
