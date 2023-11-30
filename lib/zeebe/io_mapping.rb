3e45f476ceea9e79e46d3481920a67a9cdce04c4
Elijah Miller <elijah.miller@gmail.com>
2023-11-30 12:06:32 -0500

    Minor cleanup.

--- lib/zeebe/io_mapping.rb
+++ lib/zeebe/io_mapping.rb
@@ -2,11 +2,17 @@
 
 module Zeebe
   class IoMapping
-    attr_accessor :inputs, :outputs
-
     def initialize(moddle)
-      @inputs = moddle["inputParameters"]&.map { |parameter_moddle| Parameter.new(parameter_moddle) }
-      @outputs = moddle["outputParameters"]&.map { |parameter_moddle| Parameter.new(parameter_moddle) }
+      @inputs = moddle["inputParameters"] || []
+      @outputs = moddle["outputParameters"] || []
+    end
+
+    def inputs
+      @inputs.map { |parameter_moddle| Parameter.new(parameter_moddle) }
+    end
+
+    def outputs
+      @outputs.map { |parameter_moddle| Parameter.new(parameter_moddle) }
     end
   end
 end
