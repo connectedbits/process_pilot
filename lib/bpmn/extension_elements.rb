1dd5dd084ed961e9c229ad3c67faa36dc33c6442
Elijah Miller <elijah.miller@gmail.com>
2023-11-30 11:27:22 -0500

    Error on unsupported extension namespaces.

--- lib/bpmn/extension_elements.rb
+++ lib/bpmn/extension_elements.rb
@@ -2,6 +2,8 @@
 
 module Bpmn
   class ExtensionElements
+    VALID_EXTENSION_NAMESPACES = %w[zeebe]
+
     attr_accessor :assignment_definition, :called_decision, :form_definition, :io_mapping, :properties, :script, :subscription, :task_definition, :task_schedule
 
     def initialize(moddle)
@@ -12,6 +14,8 @@ module Bpmn
           moddle_value["properties"].each { |property_moddle| @properties[property_moddle["name"]] = property_moddle["value"] } if moddle_value["properties"].present?
         else
           extension_parts = extension_type.split(":")
+          namespace = extension_parts.first
+          raise "Unsupported extension namespace: #{namespace}" unless VALID_EXTENSION_NAMESPACES.include?(namespace)
           if klass = "#{extension_parts.first.capitalize}::#{extension_parts.last}".safe_constantize
             send("#{extension_parts.last.underscore}=", klass.new(moddle_value))
           else
